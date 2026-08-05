a73a2a07f2d0cc82c6967fc58a41d4c66c97d004
Deepal <deepalece@gmail.com>
2026-08-06 03:50:04 +0530

    Configure WhiteNoise for static files

--- eventmanagement/settings.py
+++ eventmanagement/settings.py
@@ -59,6 +59,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+   
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -145,3 +147,5 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
