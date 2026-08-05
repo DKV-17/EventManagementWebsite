155f1e96580c2bea2897171f4c49365664b35037
Deepal <deepalece@gmail.com>
2026-08-05 23:51:07 +0530

    Prepare project for Render deployment

--- eventmanagement/settings.py
+++ eventmanagement/settings.py
@@ -23,9 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k59l@&o&%y1v@pb(j5ex^w6-#06@r4lont7_f=1#y%#117pxu9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    ".onrender.com",
+]
 
 
 # Application definition
