c5fc911888925515a9336d0a0d04b920736e8227
Deepal <deepalece@gmail.com>
2026-08-06 20:01:09 +0530

    Added SMTP email integration

--- contact/views.py
+++ contact/views.py
@@ -1,20 +1,70 @@
 from django.shortcuts import render, redirect
 from .models import Contact
+from django.core.mail import send_mail
+from django.conf import settings
+from django.contrib import messages
+
 
 def contact(request):
 
     if request.method == "POST":
 
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        phone = request.POST.get("phone")
+        subject = request.POST.get("subject")
+        message = request.POST.get("message")
+
+        # Save to database
         Contact.objects.create(
-
-            name=request.POST.get("name"),
-            email=request.POST.get("email"),
-            phone=request.POST.get("phone"),
-            subject=request.POST.get("subject"),
-            message=request.POST.get("message")
-
+            name=name,
+            email=email,
+            phone=phone,
+            subject=subject,
+            message=message
         )
 
+        # Email to Admin
+        send_mail(
+            subject=f"New Contact Enquiry: {subject}",
+            message=f"""
+A new enquiry has been received.
+
+Name: {name}
+Email: {email}
+Phone: {phone}
+
+Message:
+{message}
+""",
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[settings.EMAIL_HOST_USER],
+            fail_silently=False,
+        )
+
+        # Confirmation Email to User
+        send_mail(
+            subject="Thank You for Contacting Make Events",
+            message=f"""
+Dear {name},
+
+Thank you for contacting Make Events.
+
+We have received your enquiry successfully.
+
+Our team will get back to you shortly.
+
+Regards,
+Make Events Team
+""",
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[email],
+            fail_silently=False,
+        )
+        messages.success(
+    request,
+    "Thank you for contacting Make Events. We have received your enquiry successfully. Our team will get back to you shortly."
+)
         return redirect("contact")
 
     return render(request, "contact/contact.html")
--- eventmanagement/settings.py
+++ eventmanagement/settings.py
@@ -165,3 +165,17 @@ MEDIA_ROOT = BASE_DIR / "media"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
+# ================= SMTP Email Configuration =================
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = "deepalece@gmail.com"
+EMAIL_HOST_PASSWORD = "artm rxdq qefk hfpb"
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
